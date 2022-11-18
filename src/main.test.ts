96475666ea31da184572a0695e3a2873e5f3c750
Bruno Ochotorena <bruno.verediano@calindra.com.br>
2022-11-18 17:04:48 -0300

    test: some changes

--- src/main.test.ts
+++ src/main.test.ts
@@ -5,7 +5,7 @@ function sum(a: number, b: number): number {
   return a + b;
 }
 
-describe('sum two numbers', () => {
+describe('portal', () => {
   it('should return the sum of two numbers', () => {
     assert.strictEqual(sum(1, 2), 3);
   });
