cede64605ac5132598324595db84433198a6bd5a
Fabio Oshiro <fabio.oshiro@gmail.com>
2022-12-05 12:51:19 -0300

    add program basic tests

--- test/solana/Program.spec.ts
+++ test/solana/Program.spec.ts
@@ -14,11 +14,20 @@ describe('Program', () => {
         factory = new Factory(config)
     })
 
-    it('should instantiate a Program', async () => {
+    it('should instantiate a Program with a signer', async () => {
         const [signer] = await ethers.getSigners();
         const idl = JSON.parse(fs.readFileSync(path.join(__dirname, './fixtures/anchor/idl/solana_twitter.json'), 'utf-8'));
         const { program } = factory.getWorkspace<SolanaTwitter>({ idl, signer });
         expect(program).to.have.property('methods');
         expect(program.methods).to.have.property('sendTweet');
+        expect(program.methods).to.have.property('deleteTweet');
+    })
+
+    it('should instantiate a Program without a signer', async () => {
+        const idl = JSON.parse(fs.readFileSync(path.join(__dirname, './fixtures/anchor/idl/solana_twitter.json'), 'utf-8'));
+        const { program } = factory.getWorkspace<SolanaTwitter>({ idl });
+        expect(program).to.have.property('methods');
+        expect(program.methods).to.have.property('sendTweet');
+        expect(program.methods).to.have.property('deleteTweet');
     })
 })
